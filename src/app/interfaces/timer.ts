7b72f240b851593a363c44c3674751b3a47f7cfc
unknown <golotlatia@gmail.com>
2022-01-11 23:32:52 +0100

    cambios en la pagina timers

---
+++ src/app/interfaces/timer.ts
@@ -0,0 +1,14 @@
+export interface timer {
+  nombre: string;
+  horaInicioHoras: string;
+  horaInicioMinutos: string;
+  horaFinalHoras: string;
+  horaFinalMinutos: string;
+  lunes: boolean;
+  martes: boolean;
+  miercoles: boolean;
+  jueves: boolean;
+  viernes: boolean;
+  sabado: boolean;
+  domingo: boolean;
+}
--- src/app/timer-page/timer-page.component.ts
+++ src/app/timer-page/timer-page.component.ts
@@ -1,15 +1,36 @@
 //Javier Zapatero Santos TFG
 import { NgxMaterialTimepickerTheme } from './../../../node_modules/ngx-material-timepicker/src/app/material-timepicker/models/ngx-material-timepicker-theme.interface.d';
 import { Component, OnInit } from '@angular/core';
+import { timer } from '../interfaces/timer';
 
 @Component({
   selector: 'app-timer-page',
   templateUrl: './timer-page.component.html',
   styleUrls: ['./timer-page.component.css'],
 })
+
+
+
+
 export class TimerPageComponent implements OnInit {
   public selectedTime = '18:33';
 
+  private programa: timer = {
+
+
+    nombre: '',
+    horaFinalMinutos: '',
+    horaFinalHoras: '',
+    horaInicioMinutos: '',
+    horaInicioHoras: '',
+    lunes: false,
+    martes: false,
+    miercoles: false,
+    jueves: false,
+    viernes: false,
+    sabado: false,
+    domingo: false
+  };
 
   darkTheme: NgxMaterialTimepickerTheme = {
     container: {
@@ -26,9 +47,45 @@ export class TimerPageComponent implements OnInit {
     }
 };
 
+lunes() {
+  this.programa.lunes = !this.programa.lunes;
+  console.log(this.programa.lunes);
+}
+martes() {
+  this.programa.martes = !this.programa.martes;
+  console.log(this.programa.martes);
+}
+miercoles() {
+  this.programa.miercoles = !this.programa.miercoles;
+  console.log(this.programa.miercoles);
+}
+jueves() {
+  this.programa.jueves = !this.programa.jueves;
+  console.log(this.programa.jueves);
+}
+viernes() {
+  this.programa.viernes = !this.programa.viernes;
+  console.log(this.programa.viernes);
+}
+sabado() {
+  this.programa.sabado = !this.programa.sabado;
+  console.log(this.programa.sabado);
+}
+domingo() {
+  this.programa.domingo = !this.programa.domingo;
+  console.log(this.programa.domingo);
+}
+
+
+
+
+
   constructor() {}
 
   ngOnInit(): void {
+
+    console.log(this.programa);
+    
 
   
   }
